848842465e237c097973b33a7fd311e896c8c131
Kapil Chouhan <kpl.chouhan99@gmail.com>
2020-01-24 22:09:47 +0530

    minor fixes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_17_143402) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scores", force: :cascade do |t|
     t.string "name"
     t.integer "score"
